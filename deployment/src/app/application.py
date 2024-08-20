4896d6d1ccd8f0183251831c63b4e6481590e058
Romuald86github <romualdtcheutchoua@gmail.com>
2024-08-20 10:27:45 +0100

    update ports in dockerfile and application

--- deployment/src/app/application.py
+++ deployment/src/app/application.py
@@ -53,4 +53,5 @@ def predict():
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=5010)
+    # No need to specify the port here. Let the environment handle it.
+    application.run(host='0.0.0.0')
